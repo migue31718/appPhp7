e6c84681d8e96b13b0e746027d6b9323bb6c691f
migue31718 <58535447+migue31718@users.noreply.github.com>
2019-12-05 21:32:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,73 +2,75 @@
 <html lang="es-mx">
 <head>
     <meta charset="UTF-8">
-    <title>IntroPhp7 Grupo XA</title>
-    <link rel="stylesheet" href="css/estilos.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Science Technology Engineerig & Mathematics</title>
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="css/estilos.css">
     <link rel="stylesheet" href="css/menu.css"/>
     <link rel="stylesheet" href="css/problema.css"/>
     <link href="https://fonts.googleapis.com/css?family=Rambla" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+    <script>
+        function solucion1(){
+        var masa=0.700;
+        var Volumen = 2.587;
+        var D=(masa/Volumen)
+        var a=Math.round(D *1000)/1000;
+        var d=document.getElementById("resultado");
+        d.innerHTML='<h2>Resultado: </h2>  La densidad en SI(sistema internacional) es: </br> D = '+ a +'  Kg/m^3 </br>';
+        }
+    </script>
 </head>
 <body>
-   <section class="wrapper">
-    <header>
-      <h1 class="logo"><a href="index.php">IntroPHP7</a></h1>
-      <nav>
-      <ul>
-        <li><a href="index.php" class="current">Inicio</a></li>
-        <li><a href="#">Otros Ejemplos Php7</a></li>
-      </ul>
-      </nav>
+    <section id="contenedor">
+    <header id="gridHeader">
+    <h1>Science Technology Engineerig & Mathematics</h1>
+    <h2>Resolver problemas de Ciencia e Ingeniería con JS</h2>
+    <h2>Nombre: Miguel angel Popoca  Naves    , No. de control: 17091136</h2>
+    <img src="images/fotoSelfie.jpg" alt="su foto">
     </header>
-<section id="contenedor">
-<section  class="problema">
-  <h2>Problema: Cálcular la densidad de la Tierra</h2>
-  <p>Descripción:</p>
-  <p>El radio de la Tierra es 6 378 km y su masa es 5.97E24 kg. <br>
-a) ¿Cuál es la densidad promedio de la Tierra?<br>
-b) Si el radio de la Tierra se redujera a una tercera parte y su masa no cambiará, cuál serı́a su densidad?<br>
-c) Si el radio de la Tierra y su masa se redujera a una
-tercera parte, ¿su densidad serı́a la misma? ¿por qué?</p>
-</section>
-<section class="formulas">
-<h2>Fórmulas</h2>
-    Densidad = Masa/Volumen<br>
-    Volumen=(4/3)pi*r*r*r
-</section>
-<section class="datos">
-<h2>Datos:</h2>
-   m= 5.97E24 Kg. <br>
-   r= 6378 Km.
-</section>
-<section class="calculos">
-<h2>Solución</h2>
-   <p>a)La densidad de la Tierra es:<br>
-    densidad = 5.97E24 Kg/volumen (m) <br>
-    volumen= (4/3)*pi*(6378000)*(6378000)*(6378000)</p>
-</section>
-<?php
-     function calcula_densidad(){
-        $PI=3.141592;
-        $masa= 5.97E24;
-        $radio= 6378e3;
-        $volumen= (4/3)*$PI*$radio*$radio*$radio;
-        $densidad= $masa/$volumen;
-        return $densidad;
-     }
-?>
-<section class="resultado">
-<h2>Resultado:</h2>
-<div id="resultadoA"></div>
-<?php
- print "<h1> resultado: densidad = ".calcula_densidad(). " kg/(metro cubico)</h1>";
-?>
-</section>
-</section>
-    <footer class="pie">
-     Creative Commons versión 3.0 SciSoft 2019
+    </section>
+    <section id="problema"  class="gridProblema">
+      <section id=descripcion>
+        <h2>Problema: Dado el siguiente caso, resolverlo modificando el codigo de JS base.</h2>
+        <p>Descripción:</p>
+        <p>Un cuerpo solido de cierto material se mide su masa y se encontro un valor de 700gr al medir su volumen este fue de 2587cm3. <br>
+            <br>
+            calcular la densidad en el sistema internacional de unidades. 
+           <br> 
+            
+            
+      </section>
+      <section id="datos">
+            <h2>Datos:</h2>
+            Masa = 700gr = 0.700kg <br>
+            Volumen = 2,587 cm^3  = 2.587 m^3  <br>
+            <br>
+      </section>
+      <section id="formulas">
+            <h2>Fórmulas</h2>
+            D = M/V <br>
+            
+
+      </section>
+      <section id="solucion">
+            <h2>Cálculos</h2>
+            <p>La densidad en SI(sistema internacional) es:<br>
+                D = (0.700)Kg / (2.587)m^3 <br>
+            D = ?
+            <br>
+            </p>
+             <button onclick="solucion1()">Presiona para calcular</button>
+             <br>
+      </section>
+      <section id="resultado">
+          <h2>Resultado</h2>
+            
+      </section>
+    </section>
+    <footer id="gridPie">
+        <p>Solution Science Problems. 2019 Creative Commons 3.0</p>
     </footer>
-   </section>
 </body>
 </html>
-
-
